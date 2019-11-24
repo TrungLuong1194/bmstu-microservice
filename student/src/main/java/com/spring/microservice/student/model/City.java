26714a50cf0f9bc57581e7ba0a641b8205515a0a
TrungLuong1194 <trungluong1194@gmail.com>
2019-11-25 00:41:36 +0300

    add test for city

--- student/src/main/java/com/spring/microservice/student/model/City.java
+++ student/src/main/java/com/spring/microservice/student/model/City.java
@@ -18,6 +18,11 @@ public class City {
         this.name = name;
     }
 
+    public City(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public long getId() {
         return id;
     }
