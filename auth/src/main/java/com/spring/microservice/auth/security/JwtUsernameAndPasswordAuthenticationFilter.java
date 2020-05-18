5a0e8fd7188de394caa8d58d964ddd54f410ba11
TrungLuong1194 <trungluong1194@gmail.com>
2020-05-18 22:25:47 +0300

    update

--- auth/src/main/java/com/spring/microservice/auth/security/JwtUsernameAndPasswordAuthenticationFilter.java
+++ auth/src/main/java/com/spring/microservice/auth/security/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -23,83 +23,87 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter   {
-	
-	// We use auth manager to validate the user credentials
-	private AuthenticationManager authManager;
-	
-	private final JwtConfig jwtConfig;
-    
-	public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authManager, JwtConfig jwtConfig) {
-		this.authManager = authManager;
-		this.jwtConfig = jwtConfig;
-		
-		// By default, UsernamePasswordAuthenticationFilter listens to "/login" path. 
-		// In our case, we use "/auth". So, we need to override the defaults.
-		this.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher(jwtConfig.getUri(), "POST"));
-	}
-	
-	@Override
-	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-			throws AuthenticationException {
-		
-		try {
-			
-			// 1. Get credentials from request
-			UserCredentials creds = new ObjectMapper().readValue(request.getInputStream(), UserCredentials.class);
-			
-			// 2. Create auth object (contains credentials) which will be used by auth manager
-			UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-					creds.getUsername(), creds.getPassword(), Collections.emptyList());
-			
-			// 3. Authentication manager authenticate the user, and use UserDetialsServiceImpl::loadUserByUsername() method to load the user.
-			return authManager.authenticate(authToken);
-			
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	// Upon successful authentication, generate a token.
-	// The 'auth' passed to successfulAuthentication() is the current authenticated user.
-	@Override
-	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-			Authentication auth) throws IOException, ServletException {
-		
-		Long now = System.currentTimeMillis();
-		String token = Jwts.builder()
-			.setSubject(auth.getName())	
-			// Convert to list of strings. 
-			// This is important because it affects the way we get them back in the Gateway.
-			.claim("authorities", auth.getAuthorities().stream()
-				.map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
-			.setIssuedAt(new Date(now))
-			.setExpiration(new Date(now + jwtConfig.getExpiration() * 1000))  // in milliseconds
-			.signWith(SignatureAlgorithm.HS512, jwtConfig.getSecret().getBytes())
-			.compact();
-		
-		// Add token to header
-		response.addHeader(jwtConfig.getHeader(), jwtConfig.getPrefix() + token);
-	}
-	
-	// A (temporary) class just to represent the user credentials
-	private static class UserCredentials {
-	    private String username, password;
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-		public String getUsername() {
-			return username;
-		}
+    // We use auth manager to validate the user credentials
+    private AuthenticationManager authManager;
 
-		public void setUsername(String username) {
-			this.username = username;
-		}
+    private final JwtConfig jwtConfig;
 
-		public String getPassword() {
-			return password;
-		}
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authManager, JwtConfig jwtConfig) {
+        this.authManager = authManager;
+        this.jwtConfig = jwtConfig;
 
-		public void setPassword(String password) {
-			this.password = password;
-		}
-	}
+        // By default, UsernamePasswordAuthenticationFilter listens to "/login" path.
+        // In our case, we use "/auth". So, we need to override the defaults.
+        this.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher(jwtConfig.getUri(), "POST"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+        try {
+
+            // 1. Get credentials from request
+            UserCredentials creds = new ObjectMapper().readValue(request.getInputStream(), UserCredentials.class);
+
+            // 2. Create auth object (contains credentials) which will be used by auth manager
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    creds.getUsername(), creds.getPassword(), Collections.emptyList());
+
+            // 3. Authentication manager authenticate the user, and use UserDetialsServiceImpl::loadUserByUsername() method to load the user.
+            return authManager.authenticate(authToken);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Upon successful authentication, generate a token.
+    // The 'auth' passed to successfulAuthentication() is the current authenticated user.
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        Long now = System.currentTimeMillis();
+        String token = Jwts.builder()
+                .setSubject(auth.getName())
+                // Convert to list of strings.
+                // This is important because it affects the way we get them back in the Gateway.
+                .claim("authorities", auth.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + jwtConfig.getExpiration() * 1000))  // in milliseconds
+                .signWith(SignatureAlgorithm.HS512, jwtConfig.getSecret().getBytes())
+                .compact();
+
+        // Add token to header
+        response.addHeader(jwtConfig.getHeader(), jwtConfig.getPrefix() + token);
+
+        final String origin = "http://localhost:63343";
+
+        response.addHeader("Access-Control-Allow-Origin", origin);
+    }
+
+    // A (temporary) class just to represent the user credentials
+    private static class UserCredentials {
+        private String username, password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
 }
--- gateway/src/main/java/com/spring/microservice/gateway/security/SecurityTokenConfig.java
+++ gateway/src/main/java/com/spring/microservice/gateway/security/SecurityTokenConfig.java
@@ -10,36 +10,52 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableWebSecurity 	// Enable security config. This annotation denotes config for spring security.
+import java.util.Arrays;
+
+@EnableWebSecurity    // Enable security config. This annotation denotes config for spring security.
 public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private JwtConfig jwtConfig;
- 
-	@Override
-  	protected void configure(HttpSecurity http) throws Exception {
-    	   http
-		.csrf().disable()
-		    // make sure we use stateless session; session won't be used to store user's state.
-	 	    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) 	
-		.and()
-		    // handle an authorized attempts 
-		    .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)) 	
-		.and()
-		   // Add a filter to validate the tokens with every request
-		   .addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
-		// authorization requests config
-		.authorizeRequests()
-		   // allow all who are accessing "auth" service
-		   .antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()  
-		   // must be an admin if trying to access admin area (authentication is also required here)
-		   .antMatchers("/students" + "/admin/**").hasRole("ADMIN")
-		   // Any other request must be authenticated
-		   .anyRequest().authenticated(); 
-	}
-	
-	@Bean
-  	public JwtConfig jwtConfig() {
-    	   return new JwtConfig();
-  	}
+    @Autowired
+    private JwtConfig jwtConfig;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and()
+                .csrf().disable()
+                // make sure we use stateless session; session won't be used to store user's state.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // handle an authorized attempts
+                .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                // Add a filter to validate the tokens with every request
+                .addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+                // authorization requests config
+                .authorizeRequests()
+                // allow all who are accessing "auth" service
+                .antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+                // must be an admin if trying to access admin area (authentication is also required here)
+                .antMatchers("/students" + "/admin/**").hasRole("ADMIN")
+                // Any other request must be authenticated
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public JwtConfig jwtConfig() {
+        return new JwtConfig();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        source.registerCorsConfiguration("/**", config.applyPermitDefaultValues());
+        config.setExposedHeaders(Arrays.asList("Authorization"));
+
+        return source;
+    }
 }
